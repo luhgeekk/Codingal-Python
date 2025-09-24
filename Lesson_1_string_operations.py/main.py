8edb883cf51e996c09ff3a91226a259bbde8f3bd
luhgeekk <minalfawaz@gmail.com>
2025-09-24 17:13:18 +0100

    Lesson 1 Data types

---
+++ Lesson_1_string_operations.py/main.py
@@ -0,0 +1,11 @@
+name1 = "Minal "
+name2 = "Fawaz"
+full_name = name1+name2
+var = "ha"*10
+
+print("Full name: ",full_name)
+print(var)
+
+word = "pizza"
+print("Length of string: ",len(word))
+print("First letter of string: ",word[0])
